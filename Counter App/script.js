20ae6cc5f5921f7b7faf235b00cf9b9b7b05f5ae
suyash <sharmasuyash0505@gmail.com>
2025-04-14 11:08:32 +0530

    first commit

---
+++ Counter App/script.js
@@ -0,0 +1,22 @@
+const count = document.querySelector('.count');
+const minusBtn = document.querySelector('.minus-btn');
+const plusBtn = document.querySelector('.plus-btn');
+const changeBy = document.querySelector('.changeBy');
+const resetBtn = document.querySelector('.reset');
+
+plusBtn.addEventListener('click',()=>{
+    let currentCount = parseInt(count.innerText);
+    const changeByValue = parseInt(changeBy.value);
+    count.innerText = currentCount + changeByValue;
+});
+
+minusBtn.addEventListener('click',()=>{
+    let currentCount = parseInt(count.innerText);
+    const changeByValue = parseInt(changeBy.value);
+    count.innerText = currentCount-changeByValue;
+});
+
+resetBtn.addEventListener('click',()=>{
+    count.innerText = 0;
+    changeBy.value = 1;
+});
